73492b8c305b2d61805f889f6dd08dafba6802fd
louay0khaled <louay.o.khaled@gmail.com>
2026-09-09 23:45:23 +0300

    Use published KSP version compatible with current Kotlin build

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,5 +8,6 @@ plugins {
     id("com.android.application") version "9.3.1" apply false
     id("com.android.library") version "9.3.1" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
-    id("com.google.devtools.ksp") version "2.4.20-1.0.29" apply false
+    id("org.jetbrains.kotlin.android") version "2.4.20" apply false
+    id("com.google.devtools.ksp") version "2.3.10" apply false
 }
