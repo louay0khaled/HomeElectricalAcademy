7f9600cfad34f07c4d8d1a4a3387ffe8a9303d80
louay0khaled <louay.o.khaled@gmail.com>
2026-09-09 15:17:04 +0300

    build: add electrical simulator library module

---
+++ core/simulator/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+android { namespace = "com.louaykhaled.homeelectricalacademy.core.simulator"; compileSdk = 37
+    defaultConfig { minSdk = 26 }
+    kotlinOptions { jvmTarget = "17" }
+}
+
+dependencies {
+    implementation(project(":core:model"))
+}
